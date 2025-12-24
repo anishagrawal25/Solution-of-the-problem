80a45f2a2f923663e0993bb03ef68acf8e49c3f0
anishagrawal25 <anishasa525@gmail.com>
2025-12-24 12:37:27 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,3 +1,4 @@
+#Given an array nums of n integers, return true if the array nums is sorted in non-decreasing order or else false.
 def is_sorted(arr):
     for i in range(len(arr)-1):
         if arr[i]>arr[i+1]:
@@ -6,6 +7,7 @@ def is_sorted(arr):
 
 n = int(input())
 arr = list(map(int,input().split()))
+
 
 
 print(is_sorted(arr))
