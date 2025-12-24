692d3dca004be5d72e101f5c33a10b0a01f2c439
anishagrawal25 <anishasa525@gmail.com>
2025-12-24 12:36:21 +0530

    Update py.py

--- py.py
+++ py.py
@@ -1,7 +1,9 @@
+Given an array nums of n integers, return true if the array nums is sorted in non-decreasing order or else false.
 class Solution:
     def printNumber(self):
         num = int(input())
         print(num)
 
 sol=Solution()
+
 sol.printNumber()
