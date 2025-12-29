7d2759d5738d605fea634b3eee4118daf008ad76
anishagrawal25 <anishasa525@gmail.com>
2025-12-29 11:08:51 +0530

    Add files via upload
    https://takeuforward.org/plus/dsa/problems/sort-an-array-of-0's-1's-and-2's

--- sort.py
+++ sort.py
@@ -1,13 +1,14 @@
-#Given an array nums of n integers, return true if the array nums is sorted in non-decreasing order or else false.
-def is_sorted(arr):
-    for i in range(len(arr)-1):
-        if arr[i]>arr[i+1]:
-            return False
-    return True
-
-n = int(input())
-arr = list(map(int,input().split()))
-
-
-
-print(is_sorted(arr))
+n=int(input())
+arr=list(map(int,input().split()))
+low, mid, high=0,0,len(arr)-1
+while mid<=high:
+    if arr[mid]==0:
+        arr[low],arr[mid]=arr[mid],arr[low]
+        low+=1
+        mid+=1
+    elif arr[mid]==1:
+        mid+=1
+    else:
+        arr[mid],arr[high]=arr[high],arr[mid]
+        high-=1
+print(*arr)
